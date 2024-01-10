e31e3b363973b7996a77889911f925e68050f8cc
Toby Schrapel <toby@tobyschrapel.com>
2024-01-09 22:10:28 -0600

    chore: switch to @vercel/style-guide for prettier

--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -1,14 +1,14 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-    parameters: {
-        actions: { argTypesRegex: '^on[A-Z].*' },
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        },
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
     },
-}
+  },
+};
 
-export default preview
+export default preview;
